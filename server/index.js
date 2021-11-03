f1c8de1e37d99a167e9d65572502558089a7ba62
grigory-sj-ninja <grigory@maroo.us>
2021-11-03 15:17:44 +0300

    feat: add subscription server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { BUSINESS } = require('./data/business')
+const express = require('express')
+const  { createServer } = require('http');
+const { execute, subscribe } = require('graphql');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 const typeDefs = gql`
   type Payment {
@@ -25,6 +30,10 @@ const typeDefs = gql`
   type Query {
     paymentRequests: [PaymentRequest]
   }
+
+  type Subscription {
+    paymentCreated: Payment
+  }
 `;
 
 const resolvers = {
@@ -33,9 +42,39 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const app = express()
 
+const httpServer = createServer(app);
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const port = 8000;
 
-server.listen(8000).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+const server = new ApolloServer({
+  schema,
+  plugins: [{
+    async serverWillStart() {
+      return {
+        async drainServer() {
+          subscriptionServer.close();
+        }
+      };
+    }
+  }],
+});
+
+const subscriptionServer = SubscriptionServer.create({
+  schema,
+  execute,
+  subscribe,
+}, {
+  server: httpServer,
+  path: server.graphqlPath,
+});
+
+server.listen(port, () => {
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`,
+  );
+  console.log(
+    `🚀 Subscriptions ready at ws://localhost:${port}${subscriptionServer.wsServer.options.path}`,
+  );
 });
