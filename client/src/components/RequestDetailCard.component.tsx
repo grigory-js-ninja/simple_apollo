acc4017b028d2cb2e874dfec0c98ac46d6a44d51
grigory-sj-ninja <grigory@maroo.us>
2021-11-04 13:34:11 +0300

    fix: add more details

--- client/src/components/RequestDetailCard.component.tsx
+++ client/src/components/RequestDetailCard.component.tsx
@@ -1,16 +1,17 @@
 import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 import { IRequest } from '../types/request'
+
 export function RequestDetailCard(props: IRequest) {
-  console.log(props);
-  
+
   return (
     <Card>
       <Card.Body>
         <Card.Title>{props.id}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
-        </Card.Text>
+        <p>Business: {props.businessId}</p>
+        <p>Client: {props.clientId}</p>
+        <p>Note: {props.note}</p>
+        <p>Link: {props.shortCode}</p>
+        <p>Total Amount: {props.totalAmount}</p>
       </Card.Body>
       <ListGroup className="list-group-flush">
         {props.payments.map((p, idx) => {
@@ -24,14 +25,7 @@ export function RequestDetailCard(props: IRequest) {
             </ListGroupItem>
           )
         })}
-        {/* <ListGroupItem>Cras justo odio</ListGroupItem>
-        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-        <ListGroupItem>Vestibulum at eros</ListGroupItem> */}
       </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
     </Card>
   )
 }
